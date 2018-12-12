90110b464abcefbdc8cd8b4f02a81b327d71929a
Luigi Muller <sluigimuller@gmail.com>
2018-12-12 15:59:08 -0400

    Código

--- Arduino/Codigo_sistema/Codigo_sistema.ino
+++ Arduino/Codigo_sistema/Codigo_sistema.ino
@@ -11,15 +11,42 @@ estadoD = com energia da rua e com carga total na bateria*/
 ACS712 sensor(ACS712_30A, A0);
 
 int tensao_bateria = 9; // tensao em Volts;
-int capacidade_bateria = 250; //capacidade em mA;
-int carga_atual = capacidade_bateria;
+int capacidade_bateria = 250; //capacidade em mAh;
+int carga_atual = capacidade_bateria; // carga em porcentagem;
 int recarregador_bateria = 20; // quanto a placa solar recarrega, em mA;
+int taxa_consumo = 50; //taxa em mA
 
 //Porta ligada ao pino IN1 do modulo
-int porta_rele_energiaRua= 2;
+int porta_rele_energiaRua = 2;
 //Porta ligada ao pino IN2 do modulo
 int porta_rele_placaSolar = 4;
 int estadoAtual = estadoA; //estado inicial
+
+float I = 0;
+
+void imprime_dados(){
+  Serial.print("Corrente da rua: ");
+  Serial.print(I, 4);
+  Serial.println("A");
+
+  Serial.print("Carga Atual: ");
+  Serial.print( (carga_atual/capacidade_bateria)*100, 3);
+  Serial.println("%");
+}
+
+void tempo_recarga(){
+  Serial.print("Tempo estimado de recarga: ");
+  Serial.print((capacidade_bateria - carga_atual)/recarregador_bateria, 3);
+  Serial.println("horas");
+  carga_atual = carga_atual + recarregador_bateria;
+}
+
+void tempo_uso(){
+  Serial.print("Tempo estimado de uso: ");
+  Serial.print((capacidade_bateria - carga_atual)/taxa_consumo, 3);
+  Serial.println("horas");
+  carga_atual = carga_atual - taxa_consumo;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -30,58 +57,75 @@ void setup() {
 }
 
 void loop() {
-  float I = sensor.getCurrentAC(60);
-  digitalWrite(porta_rele_energiaRua, HIGH);  //Desliga rele 1
-  digitalWrite(porta_rele_placaSolar, LOW); //Liga rele 2
-  
-  
-  while (I <= 0.08){
-    I = sensor.getCurrentAC(60);
-    digitalWrite(porta_rele_energiaRua, LOW);  //Desliga rele 1
-    digitalWrite(porta_rele_placaSolar, HIGH); //Liga rele 2
+  I = sensor.getCurrentAC(60);
+  digitalWrite(porta_rele_energiaRua, LOW);  //Liga rele 1
+  digitalWrite(porta_rele_placaSolar, LOW); //Desliga rele 2
+  if(I <= 0.08){
+    I = 0;
   }
-  
-  Serial.println(I,5);
-  Serial.println("=============");
-  delay(1000);
+  if(carga_atual < 0){
+    carga_atual = 0;
+  }
+  imprime_dados();
 
-  switch(estado){
+  switch(estadoAtual){
     case estadoA:
-      if(I != 0.0){
-        Serial.printl("    Recarregando baterias    ");
-        //recarregar bateria
+      if(I != 0){
+        Serial.println("    Recarregando baterias    "); //recarregar bateria
+        tempo_recarga();
+        digitalWrite(porta_rele_energiaRua, HIGH);
+        digitalWrite(porta_rele_placaSolar, LOW);
         estadoAtual = estadoC;
       }
       break;
     case estadoB:
       if(carga_atual == 0){
-        Serial.printl("    OLHA O BLECAUTEEEEE    ");
+        Serial.println("    OLHA O BLECAUTEEEEE    ");
+        digitalWrite(porta_rele_energiaRua, LOW);
+        digitalWrite(porta_rele_placaSolar, LOW);
         estadoAtual = estadoA;
       } else if(I != 0){
-        Serial.printl("    Recarregando baterias    ");
-        //recarregar bateria
+        Serial.println("    Recarregando baterias    ");//recarregar bateria
+        digitalWrite(porta_rele_energiaRua, LOW);
+        digitalWrite(porta_rele_placaSolar, LOW);
+        tempo_recarga();
         estadoAtual = estadoC;
       }
       break;
     case estadoC:
       if(I == 0){
-        //ativa energia da bateria
+        Serial.println("    Usando energia das baterias    ");//ativa energia da bateria
+        digitalWrite(porta_rele_energiaRua, HIGH);
+        digitalWrite(porta_rele_placaSolar, HIGH);
+        tempo_uso();
         estadoAtual = estadoB;
       } else if(carga_atual == capacidade_bateria){
-        //ativa energia da bateria
+        Serial.println("    Usando energia das baterias    ");//ativa energia da bateria
+        digitalWrite(porta_rele_energiaRua, HIGH);
+        digitalWrite(porta_rele_placaSolar, HIGH);
+        tempo_uso();
         estadoAtual = estadoD;
       }
       break;
     case estadoD:
       if(I == 0){
-        //ativa energia da bateria
+        Serial.println("    Usando energia das baterias    ");//ativa energia da bateria
+        digitalWrite(porta_rele_energiaRua, HIGH);
+        digitalWrite(porta_rele_placaSolar, HIGH);
+        tempo_uso();
         estadoAtual = estadoB;
-      } else if(){
-        //recarregar bateria
+      } 
+      else if(carga_atual/capacidade_bateria < 0.5){
+        Serial.println("    Recarregando baterias    ");//recarregar bateria
+        digitalWrite(porta_rele_energiaRua, LOW);
+        digitalWrite(porta_rele_placaSolar, LOW);
+        tempo_recarga();
         estadoAtual = estadoC;
       }
       break;
     default:
       break;
   }
+  Serial.println("==================");
+  delay(3000);
 }
